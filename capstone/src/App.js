733b1a09cc0a3f4eee5e30384f8be03f5fcab727
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-06-02 22:33:27 -0700

    finished with reserve and confirmation page

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -2,20 +2,12 @@ import "./App.css";
 import React from "react";
 // or import { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import SharedPage from "./pages/SharedPage";
-// import Home from "./pages/Home";
-// import About from "./pages/About"; // line 11
-// import Menu from "./pages/Menu";
-// import Reserve from "./pages/Reserve";
-// import ConfirmRes from "./pages/ConfirmRes";
-// import Error from "./pages/Error";
 import Loading from "./pages/Loading";
 const LazySharedPage = React.lazy(() => import("./pages/SharedPage"));
 const LazyHome = React.lazy(() => import("./pages/Home"));
 const LazyAbout = React.lazy(() => import("./pages/About"));
 const LazyMenu = React.lazy(() => import("./pages/Menu"));
 const LazyReserve = React.lazy(() => import("./pages/Reserve"));
-const LazyConfirmRes = React.lazy(() => import("./pages/ConfirmRes"));
 const LazyError = React.lazy(() => import("./pages/Error"));
 
 function App() {
@@ -23,7 +15,6 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          {/* HOME, ABOUT, MENU, RESERVATION, ORDER ONLINE LOGIN */}
           <Route
             path="/"
             element={
@@ -62,14 +53,6 @@ function App() {
               element={
                 <React.Suspense fallback={<Loading />}>
                   <LazyReserve />
-                </React.Suspense>
-              }
-            />
-            <Route
-              path="reserve-confirm"
-              element={
-                <React.Suspense fallback={<Loading />}>
-                  <LazyConfirmRes />
                 </React.Suspense>
               }
             />

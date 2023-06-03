69827af6b6f4b32eacd315c0029bfd91a4bbbbea
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-06-02 23:28:49 -0700

    code clean up

--- src/pages/SharedPage.js
+++ src/pages/SharedPage.js
@@ -1,12 +1,10 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-// import StyledNavbar from "../components/StyledNavbar";
 import { Outlet } from "react-router-dom";
 
 const SharedPage = () => {
   return (
     <>
-      {/* <StyledNavbar /> => NavLink that knows whether or not it is active or pending */}
       <Navbar />
       <Outlet />
       <Footer />
